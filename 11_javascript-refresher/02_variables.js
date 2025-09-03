b4d9b0cd900d1dcd2e7759ecb27879dce7825f92
nicklee-almaida <almaidanicklee16@gmail.com>
2025-09-03 23:03:32 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,22 @@
+let name = "nicklee";
+let age = 21;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+console.log("5" == 5);
+console.log("5" === 5);
