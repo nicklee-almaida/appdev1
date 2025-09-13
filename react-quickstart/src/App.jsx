6590d9e86565e051189d6be4f176cd4a8d13155b
nicklee-almaida <almaidanicklee16@gmail.com>
2025-09-13 15:18:55 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
   return(
     <>
-      <h1>About</h1>
-      <p>Hello there! <br /> Don't hesitate to contact us if you are interested</p>
+      <h1 className='h1' style={{color: 'lightcoral'}}>About</h1>
+      <p className='paragraph' style={{color: 'lightsteelblue', fontSize: 25}}>Hello there! <br /> Don't hesitate to contact us if you are interested</p>
     </>
   )
 }
