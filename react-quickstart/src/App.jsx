256cd75feee9ce9d77280adfd56a218ed7f03d08
nicklee-almaida <almaidanicklee16@gmail.com>
2025-09-13 15:31:03 +0800

    Done conditional rendering

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,15 +1,28 @@
 import './App.css'
 
-const user = {
-  name: "Nicklee Almaida",
-  age: 21
+
+function Admin() {
+  return (
+    <h1>AdminPanel</h1>
+  )
 }
+function LogInForm() {
+  return (
+    <h1>Log in form</h1>
+  )
+}
+
 function App() {
-  return(
-    <>
-      <h1 className='h1' style={{color: 'lightcoral'}}>My Info</h1>
-      <p className='paragraph' style={{color: 'black', fontSize: 20, textAlign: 'start'}}>Hello my name is {user.name} <br /> I am {user.age} years old</p>
-    </>
+  
+  let content;
+  const isLoggedIn = true;
+  if (isLoggedIn) {
+    content = <Admin />
+  } else {
+    content = <LogInForm />
+  }
+  return (
+    <p>{ content }</p>
   )
 }
 export default App
