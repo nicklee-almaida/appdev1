d95dceaa69a1d39f9ff8595dce8b6e627834161c
nicklee-almaida <almaidanicklee16@gmail.com>
2025-09-13 15:02:38 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,17 +1,10 @@
-
 import './App.css'
-
- function MyButton() {
-  return (
-    <button>I'm a button</button>
-  )
- }
 
 function App() {
   return(
     <>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <h1>About</h1>
+      <p>Hello there! <br /> Don't hesitate to contact us if you are interested</p>
     </>
   )
 }
