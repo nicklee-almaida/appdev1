51030f3bc31c833eb2adcb02d30d2f3b091a6fb1
nicklee-almaida <almaidanicklee16@gmail.com>
2025-09-13 15:26:14 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 
+const user = {
+  name: "Nicklee Almaida",
+  age: 21
+}
 function App() {
   return(
     <>
-      <h1 className='h1' style={{color: 'lightcoral'}}>About</h1>
-      <p className='paragraph' style={{color: 'lightsteelblue', fontSize: 25}}>Hello there! <br /> Don't hesitate to contact us if you are interested</p>
+      <h1 className='h1' style={{color: 'lightcoral'}}>My Info</h1>
+      <p className='paragraph' style={{color: 'black', fontSize: 20, textAlign: 'start'}}>Hello my name is {user.name} <br /> I am {user.age} years old</p>
     </>
   )
 }
